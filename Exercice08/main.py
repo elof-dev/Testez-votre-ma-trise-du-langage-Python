e724f45b2613d09ce6dfbc7f5f8737ed805d15fb
elof-dev <elodie.fourcade65@gmail.com>
2025-04-10 07:48:01 +0200

    Update main.py

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,6 +1,11 @@
 def log_decorator(func):
-     pass
- 
+    def wrapper():
+        print("Avant la fonction.")
+        result = func()
+        print("Après la fonction.")
+        return result
+    return wrapper
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
