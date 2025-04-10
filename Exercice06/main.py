e6c72ee0c8deb96eb13a1831cc7c853bb17db5a2
elof-dev <elodie.fourcade65@gmail.com>
2025-04-10 07:27:13 +0200

    Update main.py

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,5 +1,6 @@
 # Fonction calculate_average
- 
+def calculate_average(numbers):
+   return sum(numbers) / len(numbers)
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 average = calculate_average(numbers)
