357d60f97b19a8be98e20b5b4f9303068d0d3efb
elof-dev <elodie.fourcade65@gmail.com>
2025-04-10 07:59:13 +0200

    Update main.py

--- Exercice10/main.py
+++ Exercice10/main.py
@@ -1,1 +1,16 @@
-## Écrivez votre code ici !
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def display_details(self):
+        super().display_details()
+        print(f"Salary: {self.salary}")## Écrivez votre code ici !
