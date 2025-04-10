f23c074d03e77f6ad2a6aafb3769c202136357d4
elof-dev <elodie.fourcade65@gmail.com>
2025-04-10 07:25:15 +0200

    Update main.py

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,7 @@
+'''Fonctions d'addition contenant des paramètres a et b'''
 def sum(a, b):
     return a + b
- 
+
+'''Fonctions de soustraction contenant des paramètres a et b'''
 def subtraction(a, b):
     return a - b
