2ab908a70624249ca06ce48408d268e0623646a1
elof-dev <elodie.fourcade65@gmail.com>
2025-04-10 07:36:59 +0200

    Update main.py

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,4 @@
-## Écrivez votre code ici !
+def square(number):
+    if type(number) not in (int, float):
+        raise TypeError("Le paramètre doit être un nombre !")
+    return number * number## Écrivez votre code ici !
