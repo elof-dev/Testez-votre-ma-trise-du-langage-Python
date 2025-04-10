2810e2b8f59deba063a14852e99bf0b0c0c9f320
elof-dev <elodie.fourcade65@gmail.com>
2025-04-10 07:17:24 +0200

    Update main.py

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,5 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+vowels = ["a", "e", "i", "o", "u", "y"]
+
+words_vowels = [(word, sum(1 for char in word if char in vowels)) for word in words]
+print(words_vowels)
