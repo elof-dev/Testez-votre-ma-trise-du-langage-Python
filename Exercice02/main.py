8a940cfebf61387f64b86a62a7079349ea34fb3d
elof-dev <elodie.fourcade65@gmail.com>
2025-04-10 07:06:36 +0200

    Update main.py exo 2

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -15,3 +15,13 @@ students = {
          'Histoire': 78
      }
 }
+
+chosen_name = input("Entrez le nom de l’étudiant : ")
+if chosen_name in students:
+    print(f"Les notes de {chosen_name} sont :")
+    for subject, grade in students[chosen_name].items():
+        print(f"{subject} : {grade}")
+    moyenne = int(sum(students[chosen_name].values()) / len(students[chosen_name]))
+    print(f"La moyenne de {chosen_name} est : {moyenne}")
+else:
+    print(f"L'étudiant {chosen_name} n'existe pas dans la liste.")
